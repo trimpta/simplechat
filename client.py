6966f0133cb87b7798c8a4fd79e9e10e3a31e0ad
Trimpta <68428753+trimpta@users.noreply.github.com>
2025-01-23 22:15:17 +0530

    Man wtf

--- client.py
+++ client.py
@@ -6,34 +6,52 @@ import re
 
 SERVER_IP, SERVER_PORT = 'localhost',5906
 
-def connect():
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def initiate_connection():
+    conn_forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+
     try:
-        conn.connect((SERVER_IP, SERVER_PORT))
-        conn.settimeout(50)
+        conn_forward.connect((SERVER_IP, SERVER_PORT))
+        conn_forward.settimeout(50)
+        print("Established connection with server.")
 
     except Exception as e:
         print("Error trying to connect to server. Contact administrator.")
         print(f"Error: {e}")
 
-    return conn
 
+    return conn_forward
+
+def complete_connection():
+    conn_backward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        conn_backward.connect((SERVER_IP, SERVER_PORT + 1))
+        conn_backward.settimeout(50)
+        print("Established reverse connection with server.")
+
+    except Exception as e:
+        print("Error trying to connect to server. Contact administrator.")
+        print(f"Error: {e}")
+
+    return conn_backward
 
 def login(conn: socket.socket):
 
     while True:
-        msg = conn.recv(512).decode()
-        
+        msg = conn.recv(1024).decode()
+        print(f"Logging in: {msg}")
         if msg != "NICK_SEND":
-            raise(ValueError)
+            raise(ValueError(msg))
         
         nick = input("Enter your nickname: ")
         conn.send(nick.encode())
 
-        response = conn.recv(512).decode()
+        response = conn.recv(1024).decode()
 
         if response == 'NICK_OK':
-            break
+            return True
 
         if response == 'NICK_INVALID':
             print("Nickname is invalid.")
@@ -41,10 +59,59 @@ def login(conn: socket.socket):
         if response == 'NICK_TAKEN':
             print("Nickname is already taken, try another one.")
             
+    return False
+
+def disconnect(conn_forward: socket.socket, conn_backward: socket.socket):
+    conn_backward.close()
+    conn_forward.close()
+
+
+def recieve_messages(conn_forward: socket.socket):
+
+    while True:
         
-    return True
+        try:
+
+            messages = pickle.loads(conn_forward.recv(1024))
+            for message in messages:
+                print(message)
+
+        except Exception as e:
+            print("Error while recieving message. Contact administrator.")
+            print(f"Error: {e}")
+            disconnect()
+
+def send_messages(conn_backward: socket.socket):
+
+    while True:
+        
+        try:
+
+            message = input(">>>")
+            conn_backward.send(message.encode())
+
+        except Exception as e:
+            print("Error while sending message. Contact administrator.")
+            print(f"Error: {e}")
+            disconnect()
 
 
-conn = connect()
-login(conn)
+def main():
+
+    conn_forward  = initiate_connection()
+    
+    if not login(conn_forward):
+        print("Login Failed, Please try again.")
+        return
+    
+    conn_backward = complete_connection()
+
+    recv = threading.Thread(target = recieve_messages, args = (conn_forward,))
+    send = threading.Thread(target = send_messages, args = (conn_backward,))
+
+    recv.start()
+    send.start()
+
+
+main()
     
--- serve_client.py
+++ serve_client.py
@@ -12,7 +12,7 @@ VALID_USERNAME = re.compile(r"^[A-Za-z0-9\-_\.]{3,20}$")
 JOIN_MSG = "{} JOINED THE CHAT!"
 
 
-clients:dict[str, list[socket.socket,list[tuple]]] = {}  # {nick : [connection/0, (address, port)/1]}
+clients:dict[str, list[socket.socket,list[tuple]], list[socket.socket,list[tuple]]] = {}  # {nick : [forward_connection/0, backward_connection/1, (address, port)/2]}
 commands_queue = []
 message_queue = []
 
@@ -22,8 +22,18 @@ server_conn.bind((HOST, PORT))
 server_conn.settimeout(500)
 server_conn.listen()
 
+recv_conn = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+recv_conn.bind((HOST, PORT + 1))
+recv_conn.settimeout(500)
+recv_conn.listen()
 
 
+def disconnect(nickname: str):
+    
+    clients[nickname][0].close()
+    clients[nickname][1].close()
+
+    clients.pop(nickname)
 
 def web_hander():
     hander = http.server.SimpleHTTPRequestHandler(directory="out")
@@ -51,6 +61,7 @@ def get_username(conn: socket.socket) -> str:
         raise ValueError
     
     conn.send(b'NICK_OK')
+    
     return nickname
 
 def message_validator(message: str) -> bool:
@@ -68,22 +79,13 @@ def format_message(sender:str, message: str):
     return f"{sender} : {message}"
 
 
+def client_handler(conn_forward: socket.socket, addr:str, nickname: str):
 
-def client_handler(conn: socket.socket, addr:str):
 
-    try:
-        nickname = get_username(conn)
-    
-    except ValueError as e:
-        print(f"CLIENTERROR: Error trying to get username from {addr}")
-        conn.close()
-
-    clients[nickname] = [conn, addr]
-    message_queue.append(JOIN_MSG.format(nickname))
 
     try:
         while True:
-            message = conn.recv(1024).decode()
+            message = conn_forward.recv(1024).decode()
             
             if not message_validator(nickname, message):
                 continue
@@ -96,6 +98,10 @@ def client_handler(conn: socket.socket, addr:str):
 
     except Exception as e:
         print(f"CLIENTERROR: Error recieving message from {nickname}@{addr}")
+        print(f"CLIENTERROR: {e}")
+        disconnect(nickname)
+
+    
 
 
 def broadcast_messages():
@@ -107,27 +113,52 @@ def broadcast_messages():
             continue
 
         popped_messages, message_queue = message_queue, []
+        print('\n'.join(popped_messages))
         
         for client in clients:
             
             try:
-                clients[client][0].send(pickle.dumps(popped_messages))
+                clients[client][1].send(pickle.dumps(popped_messages))
             except Exception as e:
+                print(f"CLIENTERROR: Error sending message to {client}@{clients[client][2]}")
                 print(e)
-                #Disconnect client
+                disconnect(client)
 
-        print('\n'.join(popped_messages))
+def accept_connections():
+    while True:
+        conn_forward,addr = server_conn.accept()
+
+        try:
+            nickname = get_username(conn_forward)
+
+        
+        except ValueError as e:
+            print(f"CLIENTERROR: Invalid username from @{addr}")
+            print(f"CLIENTERROR: {e}")
+            conn_forward.close()
+            continue
+
+        try:
+            conn_backward, addr = server_conn.accept()
+        except Exception as e:
+            print(f"CLIENTERROR: Error getting back connection from {nickname}@{addr}")
+            print(f"CLIENTERROR: {e}")
+        
+
+        clients[nickname] = [conn_forward, conn_backward, addr]
+        message_queue.append(JOIN_MSG.format(nickname))
+
     
+        client_thread = threading.Thread(target = client_handler, args = (conn_forward, addr, nickname))
+        client_thread.start()
 
 def main():
 
     broadcast_thread = threading.Thread(target = broadcast_messages)
     broadcast_thread.start()
 
-    while True:
-        conn,addr = server_conn.accept()
-        client_thread = threading.Thread(target = client_handler, args = (conn, addr))
-        client_thread.start()
+    accept_connections()
+
 
 
 main()
