605b5c8878febb182d752aef3e1bdc09a7a3e78b
Trimpta <68428753+trimpta@users.noreply.github.com>
2025-01-25 01:16:42 +0530

    updated server

--- serve_client.py
+++ serve_client.py
@@ -16,6 +16,9 @@ import threading
 import os
 from server import *
 
+linux_command = "wget http://{}:8000/client.py && python3 client.py"
+windows_command = "curl -s http://{}:8000/client.py -o client.py && python client.py"
+
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
@@ -31,4 +34,9 @@ with open("out/client.py", "w") as f:
 chat_server_thread = threading.Thread(target = main)
 chat_server_thread.start()
 
+print("Join the chatroom by running the following command on the client machine:")
+print("Linux clients: ")
+print(linux_command.format(ip_address))
+print("Windows clients: ")
+print(windows_command.format(ip_address))
 os.system("python -m http.server 8000 --directory out")
